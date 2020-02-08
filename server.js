294971d47c66c619b0476270e013da31a4dc0c72
naveed-rana <rana.naveed812@gmail.com>
2020-02-08 12:01:41 +0500

    docker assignment

---
+++ server.js
@@ -0,0 +1,14 @@
+//require modules
+var express = require('express');
+
+//configure app
+var app = express();
+var port = 5000;
+
+//main get request
+app.get('/test',(req,res)=>res.json("server up and running"))
+
+app.use(express.static('portfolio'))
+
+//server listen at port
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
