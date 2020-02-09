421549c8cc0accf6ec2605ff6b21dfde2ea41bac
naveed-rana <rana.naveed812@gmail.com>
2020-02-10 00:44:50 +0500

    docker assignment 1

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var express = require('express');
 
 //configure app
 var app = express();
-var port = process.env.PORT | 5000;
 
+const PORT = process.env.PORT || 5000
 //main get request
 app.get('/test',(req,res)=>res.json("server up and running"))
 
 app.use(express.static('portfolio'))
 
 //server listen at port
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
